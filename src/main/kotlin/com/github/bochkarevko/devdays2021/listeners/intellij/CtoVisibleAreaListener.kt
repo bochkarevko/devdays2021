46d002dca903072a592323a5d6757a8c23adfd6b
Kirill Bochkarev <podockonnik@gmail.com>
2021-04-28 18:00:49 +0300

    Distinguish between scrolling and dimensions changes; chill when mouse enters

--- src/main/kotlin/com/github/bochkarevko/devdays2021/listeners/intellij/CtoVisibleAreaListener.kt
+++ src/main/kotlin/com/github/bochkarevko/devdays2021/listeners/intellij/CtoVisibleAreaListener.kt
@@ -2,9 +2,17 @@ package com.github.bochkarevko.devdays2021.listeners.intellij
 
 import com.intellij.openapi.editor.event.VisibleAreaEvent
 import com.intellij.openapi.editor.event.VisibleAreaListener
+import java.awt.Rectangle
 
 class CtoVisibleAreaListener : VisibleAreaListener {
+    private fun scrolled(old: Rectangle, new: Rectangle) = (old.location != new.location)
+
     override fun visibleAreaChanged(e: VisibleAreaEvent) {
-        println("visibleAreaChanged ${e.editor.document}")
+        if (scrolled(e.oldRectangle, e.newRectangle)) {
+            println("visibleAreaChanged ${e.editor.document}")
+        }
+//        else {
+//            println("dimensions change")
+//        }
     }
 }
