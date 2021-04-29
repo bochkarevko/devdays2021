3c56ffe3dc926d770e53ac291679908804241148
Kirill Bochkarev <podockonnik@gmail.com>
2021-04-29 15:10:37 +0300

    Claim File Dialog

--- src/main/kotlin/com/github/bochkarevko/devdays2021/listeners/intellij/CtoFileEditorManagerListener.kt
+++ src/main/kotlin/com/github/bochkarevko/devdays2021/listeners/intellij/CtoFileEditorManagerListener.kt
@@ -1,13 +1,12 @@
 package com.github.bochkarevko.devdays2021.listeners.intellij
 
+import ClaimDialog
 import com.github.bochkarevko.devdays2021.MainClass
 import com.github.bochkarevko.devdays2021.utils.actionType
-import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.vfs.VirtualFile
-import java.io.File
 
 class CtoFileEditorManagerListener : FileEditorManagerListener {
     override fun fileOpened(source: FileEditorManager, file: VirtualFile) {
@@ -22,9 +21,16 @@ class CtoFileEditorManagerListener : FileEditorManagerListener {
     }
 
     override fun selectionChanged(event: FileEditorManagerEvent) {
-        event.newFile?.let { MainClass.sendAction(it.toNioPath(), actionType.SWITCH_FILE) }
+        event.newFile?.let {
+            MainClass.sendAction(it.toNioPath(), actionType.SWITCH_FILE)
+        }
+        event.oldFile?.let {
+            val fileInfo = MainClass.manager?.getFileInfo(it.toNioPath())
+            if (fileInfo != null && fileInfo.canOwn()) {
+                ClaimDialog(fileInfo.path).show()
+            }
+        }
         super.selectionChanged(event)
-        println("selectionChanged")
     }
 }
 
